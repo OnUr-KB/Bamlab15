74eed185375dcb4a30931084d87ca554dc974c25
OnUr-KB <kkanya0947@gmail.com>
2024-01-26 16:01:07 +0700

    finish

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -8,12 +8,25 @@ void shuffle(int &,int &,int &,int &);
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
-	srand(time(0));	
-	
+	srand(time(0));	 
 	for(int i = 0;i < 10;i++){
 	    shuffle(a,b,c,d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
+}
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int A[] = {a,b,c,d};
+	    for(int i = 0;i < 3 ; i++){
+		   int x = rand()%4;
+		   int y = A[i];
+		   A[i] = A[x];
+		   A[x] = y ;
+	}
+	a = A[0];
+	b = A[1];
+	c = A[2];
+	d = A[3];
 }
